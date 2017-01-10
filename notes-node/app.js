5c960ad6754e57747fe6004fec36fba4590340f8
John R. Forbes <jrforbes24@gmail.com>
2017-01-10 10:55:41 -0700

    working on nodejs site

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,16 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+  console.log("Adding a new note.");
+}
+else if (command === 'list') {
+  console.log("Listing all notes.");
+}
+else {
+  console.log("Command not recognized.");
+}
