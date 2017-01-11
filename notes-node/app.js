09364e27d4af8bcfd3f988237e1f21b6a41de825
John R. Forbes <jrforbes24@gmail.com>
2017-01-10 18:16:03 -0700

    using yargs the npm module to update notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,17 +2,26 @@ console.log('Starting apps.js!');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = argv._[0];
 console.log('Command: ', command);
+console.log("Yargs :", argv);
 
 if (command === 'add') {
-  console.log("Adding a new note.");
+  notes.addNote(argv.title, argv.body);
 }
 else if (command === 'list') {
-  console.log("Listing all notes.");
+  notes.getAll();
+}
+else if (command === 'remove') {
+  notes.removeNote(argv.title);
+}
+else if (command === 'read') {
+  notes.getNote(argv.title);
 }
 else {
   console.log("Command not recognized.");
