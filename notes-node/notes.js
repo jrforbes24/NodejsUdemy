4110117b8c5c04f7817072573b56e563a3e4a4a3
John R. Forbes <jrforbes24@gmail.com>
2017-01-31 17:00:17 -0700

    refactor notes add function

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,28 +2,32 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
-
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch (e) {
-
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
-
-  if (duplicateNotes.length === 0){
-    notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-  }
-
-};
+    if (duplicateNotes.length === 0){
+      notes.push(note);
+      saveNotes(notes);
+      return note;
+    }
+  };
 
 var getAll  = () => {
   console.log("Getting all notes.");
