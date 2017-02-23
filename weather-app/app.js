bb3036fcb9160f0798bf5c233e2b93fb993b9f8b
John R. Forbes <jrforbes24@gmail.com>
2017-02-23 14:01:06 -0700

    Learn to pull form json data

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,8 @@ const request = require('request');
 request ({
   url:'https://maps.googleapis.com/maps/api/geocode/json?address=570%20E%201925%20S%20Clearfield%20UT%2084015',
   json: true
-}, (error, reponse, body) => {
-  console.log(body);
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
