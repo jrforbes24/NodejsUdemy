0cd2b6c6cdf69935d0596a1987f340f3b52d0884
John R. Forbes <jrforbes24@gmail.com>
2017-02-28 16:16:02 -0700

    Work on getting Darksky api info

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,4 @@
+/*
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
@@ -22,7 +23,29 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) =>{
     console.log(JSON.stringify(results, undefined, 2));
   }
 });
+*/
+
+const request = require('request');
+
+request({
+  url: 'https://api.darksky.net/forecast/983174d05dc73e6ec70becf68e28a8c4/41.1333549,-112.0595603',
+  json:true
+},
+(error, response, body) => {
+  if(error){
+    console.log("Not able to contact DarkSky");
+  } else if (response.statusCode === 404) {
+    console.log('Unable to fetch weather.');
+  } else if (response.statusCode === 200 ) {
+    console.log(body.currently.temperature);
+  }
+});
 
 
 
 // https://maps.googleapis.com/maps/api/geocode/json?address=570%20E%201925%20S%20Clearfield%20UT%2084015
+
+// https://api.darksky.net/forecast/983174d05dc73e6ec70becf68e28a8c4/37.8267,-122.4233
+
+
+// 983174d05dc73e6ec70becf68e28a8c4  secret key
