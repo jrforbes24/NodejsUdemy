fe967d3fde1c0615aff483444edf7b0b0ea7d065
urcock24 <jrforbes24@gmail.com>
2017-08-08 20:25:09 -0600

    got the encoded uri working on weather-app2

--- weather-app2/app.js
+++ weather-app2/app.js
@@ -1,9 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for ',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  var the_address = encodeURIComponent(argv.address)
+
 
 request({
-  url: 'https://maps.google.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.google.com/maps/api/geocode/json?address=${the_address}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
-  console.log(JSON.stringify(results[0].formatted_address, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`lattitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`longitude: ${body.results[0].geometry.location.lng}`)
 });
