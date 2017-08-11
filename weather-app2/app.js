9ce863767b1688e830818af6df4e9948ac6daa70
urcock24 <jrforbes24@gmail.com>
2017-08-10 20:21:04 -0600

    add error handling to weather app

--- weather-app2/app.js
+++ weather-app2/app.js
@@ -21,7 +21,13 @@ request({
   url: `https://maps.google.com/maps/api/geocode/json?address=${the_address}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`lattitude: ${body.results[0].geometry.location.lat}`)
-  console.log(`longitude: ${body.results[0].geometry.location.lng}`)
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
