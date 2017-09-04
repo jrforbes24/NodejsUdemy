f6648512b81d84b63edce25dc6d6c4679a245553
urcock24 <jrforbes24@gmail.com>
2017-09-04 15:48:44 -0600

    updated the weather app to pull from darksky using lat and lng

--- weather-app2/app.js
+++ weather-app2/app.js
@@ -1,40 +1,34 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode.js')
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for ',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   }
-//   else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-const request = require('request');
+const geocode = require('./geocode/geocode.js')
+const weather = require('./weather/weather.js')
 
-request({
-  url: `https://api.darksky.net/forecast/983174d05dc73e6ec70becf68e28a8c4/41.1333549,-112.0595603`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to darksky.net servers.');
-  } else if (body.code === 400 || body.code === 404 ) {
-    console.log('Unable to fetch weather');
-  } else {
-    console.log(body.currently.temperature);
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for ',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  }
+  else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      }
+      else{
+        console.log(`It's currently: ${weatherResults.temperature} It feels like: ${weatherResults.feels_like}.`);
+      }
+    });
   }
 });
